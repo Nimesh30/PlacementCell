94da228aee2539c9557cdc1a2f248126476929c1
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-02-20 14:59:49 +0530

    created Authentication UI

--- frontend/placement-frontend/src/app/app.routes.ts
+++ frontend/placement-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './Components/home/home';
+import { Signup } from './Components/signup/signup';
+import { Login } from './Components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',component:Home},
+    {path:'signup',component:Signup},
+    {path:'login',component:Login}
+];
