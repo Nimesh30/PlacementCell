372330e1a5bd1cce78ad6daad21be809b4b24d5e
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-03-31 15:52:28 +0530

    Add email notification

--- src/main/java/com/Project/PlacementCell/Repository/StudentRepository.java
+++ src/main/java/com/Project/PlacementCell/Repository/StudentRepository.java
@@ -2,8 +2,10 @@ package com.Project.PlacementCell.Repository;
 
 import com.Project.PlacementCell.Entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +19,7 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByStudentId(String studentId);
 
     long countByPlacedTrue();
+
+    @Query("SELECT s.email FROM Student s")
+    List<String> getAllStudentEmails();
 }
